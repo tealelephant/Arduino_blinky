cafef2062f78b560170f796e24fcfc3d792689a2
tealelephant <hannahcukierman@gmail.com>
2015-09-29 10:17:47 -0500

    RUNWAY
    we changed to make it flash.

--- blinky/blinky.ino
+++ blinky/blinky.ino
@@ -1,16 +1,25 @@
 // Hannah Cukierman
 
 #define RED_LED 7
+#define YELLOW_LED 6
+#define GREEN_LED 5
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(RED_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(RED_LED, HIGH);
-  delay(1000);
+  delay(100);
   digitalWrite(RED_LED, LOW);
-  delay(1000);
+  delay(50);
+
+   digitalWrite(YELLOW_LED, HIGH);
+  delay(200);
+  digitalWrite(YELLOW_LED, LOW);
+  delay(60);
+
 }
